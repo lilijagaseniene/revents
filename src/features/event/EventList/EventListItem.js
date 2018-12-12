743d7446b0751af7dfd3472842e17d5df4addad6
Lilija Gaseniene <lilija.gaseniene@gmail.com>
2018-12-12 17:56:46 +0200

    End of section 5

--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -4,7 +4,7 @@ import EventListAtendee from './EventListAtendee';
 
 class EventListItem extends Component {
   render() {
-    const { event } = this.props;
+    const { event, onEventOpen, deleteEvent } = this.props;
     return (
       <div>
         <Segment.Group>
@@ -37,7 +37,20 @@ class EventListItem extends Component {
           </Segment>
           <Segment clearing>
             <span>{event.description}</span>
-            <Button as="a" color="teal" floated="right" content="View" />
+            <Button
+              onClick={onEventOpen(event)}
+              as="a"
+              color="teal"
+              floated="right"
+              content="View"
+            />
+            <Button
+              onClick={deleteEvent(event.id)}
+              as="a"
+              color="red"
+              floated="right"
+              content="Delete"
+            />
           </Segment>
         </Segment.Group>
       </div>
